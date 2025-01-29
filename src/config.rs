22a570737008a6e95ae703ca4f721018024ccd8b
RustDesk <71636191+rustdesk@users.noreply.github.com>
2025-01-29 16:46:59 +0800

    Update config.rs

--- src/config.rs
+++ src/config.rs
@@ -100,8 +100,6 @@ const CHARS: &[char] = &[
 pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
 pub const PUBLIC_RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
 
-pub const RS_PUB_KEY: &str = PUBLIC_RS_PUB_KEY;
-
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
 
