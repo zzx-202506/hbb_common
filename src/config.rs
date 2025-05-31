f5378e16e672cb853121dbe2e90ec98582d31022
geeinf <veelove1034@gmail.com>
2025-05-31 11:10:24 +0800

    Update config.rs

--- src/config.rs
+++ src/config.rs
@@ -100,8 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["n.wooomooo.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsrryw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["type05.top"];
+pub const RS_PUB_KEY: &str = "hCtcPVBoiNeIxxIsaAKc2A4UtV2q4JExhwaSHYYseNU=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
